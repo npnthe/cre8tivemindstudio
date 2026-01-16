71eff45939c7fbbf64f9794906b6bcceeb6a494f
npnthe <111014195+npnthe@users.noreply.github.com>
2026-01-16 20:37:58 +0300

    Add scroll animations

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+  });
+});
+
+document.querySelectorAll(".reveal").forEach(el => observer.observe(el));
